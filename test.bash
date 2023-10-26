8466b2de25a0ae621b5f815f10c9e892f3f5de0f
RE <rhenium4694@gmail.com>
2023-10-26 12:52:10 +0900

    recodo test

--- test.bash
+++ test.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Iddentifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
